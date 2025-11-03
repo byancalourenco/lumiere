560a04c06556e663093caecc85b96c79c850a376
gabrielysantos8 <gabrielysantosp7@gmail.com>
2025-11-03 10:00:58 -0300

    home

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,168 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Home.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <header>
+        <style>
+          {`@import url('https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+            @import url('https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap');`}
+        </style>
+      </header>
+
+      <main>
+        {/* logo, nome e slogan */}
+        <div className="row divlogo align-items-center">
+          <div className="logo col-md-6">
+            <img className="imglogo" src="/img/logo.png" alt="" />
+          </div>
+          <div className="col-md-6">
+            <h1 className="fontecinzel textobranco">LUMIÈRE</h1>
+            <h1 className="fontecinzel textobranco">ENTRE CAPAS E TELAS</h1>
+          </div>
+        </div>
+
+        {/* texto, botao e imagem */}
+        <div className="row align-items-center">
+          <div className="col-md-6">
+            <h5 className="diventrar textentrar textobranco">
+              Aqui você pode dar sua opinião sobre os filmes, séries e livros
+              que marcaram você ou encontrar novas histórias para se apaixonar.
+            </h5>
+            <a className="diventrar textobranco botaoentrar" href="#">
+              Entrar
+            </a>
+          </div>
+          <div className="col-md-6">
+            <img
+              className="imgcameralivros"
+              src="/img/camera e livro.png"
+              alt=""
+            />
+          </div>
+        </div>
+
+        {/* cards */}
+        <div className="grid text-center">
+          {/* CARD FILME */}
+          <div className="g-col-4 cardfilmehome h-100">
+            <img
+              className="postershome"
+              src="/img/os-caca-fantasmas.jpg"
+              alt="Os Caça-Fantasmas"
+            />
+            <h2 className="fontecinzel textobranco">OS CAÇA-FANTASMAS</h2>
+            <p className="textobranco">Valeu a pena assistir?</p>
+            <div className="grid2 text-center">
+              <p
+                className="g-col-4 textobranco"
+                style={{
+                  backgroundColor: "#678FC4",
+                  borderRadius: "30px",
+                  padding: "6px",
+                }}
+              >
+                Sim
+              </p>
+              <p
+                className="g-col-4 textobranco"
+                style={{
+                  backgroundColor: "#678FC4",
+                  borderRadius: "30px",
+                  padding: "5px",
+                  display: "inline-block",
+                  width: "150px",
+                }}
+              >
+                Mais ou menos
+              </p>
+              <p
+                className="g-col-4 textobranco"
+                style={{
+                  backgroundColor: "#678FC4",
+                  borderRadius: "30px",
+                  padding: "6px",
+                }}
+              >
+                Não
+              </p>
+            </div>
+          </div>
+
+          {/* CARD LIVRO */}
+          <div className="g-col-4 cardlivrohome h-100">
+            <img
+              className="postershome"
+              src="/img/o-pequeno-principe.jpg"
+              alt="O Pequeno Príncipe"
+            />
+            <h2
+              className="fontecinzel"
+              style={{ color: "#26364E" }}
+            >
+              O PEQUENO PRÍNCIPE
+            </h2>
+            <p className="textoazul">"Me adotou emocionalmente"</p>
+            <div style={{ display: "inline", marginBottom: "10px" }}>
+              <i className="fas fa-star" style={{ color: "#26364E" }}></i>
+              <i className="fas fa-star" style={{ color: "#26364E" }}></i>
+              <i className="fas fa-star" style={{ color: "#26364E" }}></i>
+              <i className="fas fa-star" style={{ color: "#26364E" }}></i>
+              <i className="fas fa-star" style={{ color: "#26364E" }}></i>
+            </div>
+          </div>
+
+          {/* CARD SÉRIE */}
+          <div className="g-col-4 cardseriehome h-100">
+            <div style={{ display: "inline", marginBottom: "10px" }}>
+              <i className="fas fa-star" style={{ color: "#FFFFAE" }}></i>
+              <i className="fas fa-star" style={{ color: "#FFFFAE" }}></i>
+              <i className="fas fa-star" style={{ color: "#FFFFAE" }}></i>
+              <i className="fas fa-star" style={{ color: "#FFFFAE" }}></i>
+              <i className="fas fa-star" style={{ color: "#FFFFAE" }}></i>
+            </div>
+            <div className="grid3 text-center">
+              <div
+                className="g-col-4"
+                style={{ textAlign: "justify" }}
+              >
+                <p className="textobranco">Roteiro</p>
+                <p className="textobranco">Atuação</p>
+                <p className="textobranco">Trilha Sonora</p>
+                <p className="textobranco">Fotografia</p>
+                <p className="textobranco">Originalidade</p>
+              </div>
+              <div className="g-col-4">
+                <p className="textobranco">9</p>
+                <p className="textobranco">8</p>
+                <p className="textobranco">10</p>
+                <p className="textobranco">9</p>
+                <p className="textobranco">7</p>
+              </div>
+              <div className="g-col-4">
+                <img
+                  style={{
+                    width: "100%",
+                    borderRadius: "10px",
+                    marginLeft: "5px",
+                  }}
+                  src="/img/breaking-bad.jpg"
+                  alt="Breaking Bad"
+                />
+              </div>
+            </div>
+            <h2
+              className="fontecinzel textobranco"
+              style={{ paddingTop: "25px" }}
+            >
+              BREAKING BAD
+            </h2>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
