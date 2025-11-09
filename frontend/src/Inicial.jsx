ec35a06e260ecffde0cb9d66a363a86d02a71908
gabrielysantos8 <gabrielysantosp7@gmail.com>
2025-11-09 01:09:29 -0300

    paginicial+footer

--- frontend/src/Inicial.jsx
+++ frontend/src/Inicial.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer.jsx";
+import './App.css';
 import "./Inicial.css";
 import { Link } from "react-router-dom";
 
@@ -8,13 +11,14 @@ import { Link } from "react-router-dom";
 function Inicial(){
     return(
         <div className="paginicial">
-            <style>
+            <Header />
+            <style> 
             {`@import url('https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');`}
             </style>
             <div className="container text-center">
                 <div className="row">
-                    <div className="col-md-6 col-sm-12">
-                        <h1 style={{textAlign: "left"}}>Descubra, avalie e compartilhe suas paixões!</h1>
+                    <div className="col-md-6 col-sm-12 titulo">
+                        <h1>Descubra, avalie e compartilhe suas paixões!</h1>
                     </div>
                     <div className="col-md-6 col-sm-12">
                         <img className="imginicial" src="/img/imginicial.png" alt="" />
@@ -25,37 +29,48 @@ function Inicial(){
             <div>
                 <h1 style={{textAlign: "center"}}>O que vai avaliar hoje?</h1>
 
-                    <div className="grid text-center card">
-                        <div className="g-col-4 cardopicao">
-                            <div className="grid text-center">
-                                <div className="g-col-6"><h3>Filme</h3></div>
-                                <div className="g-col-6"><img className="imgcard" src="/img/fita-de-filme.png" alt="" /></div>
+                    <div className="grid text-center cardinicial">
+                        <div className="g-col-md-4 g-col-sm-12 cardopicao h-100">
+                            <div className="row">
+                                <div className="col-6 titulocard">
+                                    <h3>Filme</h3>
+                                </div>
+                                <div className="col-6">
+                                    <img className="imgcard" src="/img/fita-de-filme.png" alt="" />
+                                </div>
                             </div>
                             <p>Avalie os filmes que marcaram você!</p>
                             <Link className="btnavaliar">Avaliar</Link>
                         </div>
 
-                        <div className="g-col-4 cardopicao">
-                            <div className="grid text-center">
-                                <div className="g-col-6"><h3>Série</h3></div>
-                                <div className="g-col-6"><img className="imgcard" src="/img/camera.png" alt="" /></div>
+                        <div className="g-col-md-4 g-col-sm-12 cardopicao h-100">
+                            <div className="row">
+                                <div className="col-6 titulocard">
+                                    <h3>Série</h3>
+                                </div>
+                                <div className="col-6">
+                                    <img className="imgcard" src="/img/camera.png" alt="" />
+                                </div>
                             </div>
                             <p>Compartilhe o que achou da sua série favorita!</p>
                             <Link className="btnavaliar">Avaliar</Link>
                         </div>
 
-                        <div className="g-col-4 cardopicao">
-                            <div className="grid text-center">
-                                <div className="g-col-6"><h3>Livro</h3></div>
-                                <div className="g-col-6"><img className="imgcard" src="/img/livro.png" alt="" /></div>
+                        <div className="g-col-md-4 g-col-sm-12 cardopicao h-100">
+                            <div className="row">
+                                <div className="col-6 titulocard">
+                                    <h3>Livro</h3>
+                                </div>
+                                <div className="col-6">
+                                    <img className="imgcard" src="/img/livro.png" alt="" />
+                                </div>
                             </div>
                             <p>Dê sua opinião sobre grandes histórias!</p>
                             <Link className="btnavaliar">Avaliar</Link>
                         </div>
                     </div>
-
             </div>
-
+            <Footer/>
         </div>
 
     )
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Footer(){
+    return(
+        <footer>
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col-md-4 col-sm-12">
+                        <p className="pfooter">Desenvolvedores:</p>
+                        <p className="pfooter">Ana Clara Rivas</p>
+                        <p className="pfooter">Beatriz Krisan</p>
+                        <p className="pfooter">Byanca Lourenço</p>
+                        <p className="pfooter">Gabriely Santos</p>
+                        <p className="pfooter">Gabriel Palozzi</p>
+                    </div>
+
+                    <div className="col-md-4 col-sm-12">
+                        <Link className="linkfooter">Obras</Link>
+                        <Link className="linkfooter">Estantes</Link>
+                        <Link className="linkfooter">Cadastrar obras</Link>
+                        <Link className="linkfooter">Perfil</Link>
+                    </div>
+                    <div className="col-md-4 col-sm-12">
+                        <img className="imgfooter img-fluid" src="./img/camera e livro.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
